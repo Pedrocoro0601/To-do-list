dc007b44f55149d4cfceed68a6102421bd1f349d
Pedro Henrique <119066915+Pedrocoro0601@users.noreply.github.com>
2023-03-02 10:02:47 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,21 +4,32 @@ var listatarefa = document.getElementById("lista-tarefas")
 var idtarefa = 0;
 var coin = document.getElementById("coin")
 
-
+/*
 var audio = new Audio('audio/mario.mp3');
 setTimeout(toca, 2000)
 function toca(){
     audio.play()
 }
-
+*/
 var botao = new Audio("audio/moeda.mp3");
+var bowser = new Audio("audio/bowser.wav")
+var princesa = new Audio("audio/princesa.wav")
+var luigi = new Audio("audio/luigi2.wav")
+var mario = new Audio("audio/soumario.mp3")
+var cano = new Audio("audio/caindocano.wav")
+var yoshi = new Audio("yoshi.wav")
 
 
+bowser.play();
+princesa.play();
+luigi.play();
+mario.play();
+cano.play();
+yoshi.play();
 
 
-
-btnaddtarefa.addEventListener("click", ()=>{
-    if (inputTarefa.value != ""){
+btnaddtarefa.addEventListener("click", () => {
+    if (inputTarefa.value != "") {
         idtarefa++;
         var li = document.createElement("li");
         li.id = idtarefa;
@@ -27,15 +38,16 @@ btnaddtarefa.addEventListener("click", ()=>{
         inputTarefa.value = "";
         botao.play();
         coin.style.animation = "moedasobe 0.5s";
-        setTimeout(()=>{coin.style.animation = ""}, 350)
+        setTimeout(() => { coin.style.animation = "" }, 350)
+
     }
-    else{
+    else {
         alert("digite uma tarefa")
     }
-    
+
 });
 
-function removertarefa(event){
+function removertarefa(event) {
     var elementoParaRemover = document.getElementById(event.target.name)
     elementoParaRemover.remove();
 }
